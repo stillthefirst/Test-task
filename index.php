8f97126fb9d9effe2d0ad2138276588570e87ebd
stillthefirst <130684497+stillthefirst@users.noreply.github.com>
2023-04-16 12:55:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Кнопка</title>
     <link href="/task/css/style.css" rel="stylesheet" type="text/css">
